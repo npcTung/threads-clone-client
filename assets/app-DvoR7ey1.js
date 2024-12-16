730b12c2a1cc6971443d4ab3f1635ee3ae8d2dfb
npcTung <tungtrinhthanh0311@gmail.com>
2024-12-16 17:51:19 +0700

    Updates

---
+++ assets/app-DvoR7ey1.js
@@ -0,0 +1,1 @@
+import{af as e,ag as o}from"./index-DusGuswS.js";const r=s=>e({url:o.ipAddress+s,method:"GET"}),a=s=>e({url:o.auth.getCredentialFormAccessToken+s,method:"GET"});export{a as g,r as i};
